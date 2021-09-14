3d4055249a751d5e467873df30c36117ac33a0ad
bpalaniswamy-aie <palaniswamybalaji64@gmail.com>
2021-09-14 10:51:10 +0530

    Division function added

--- Source.java
+++ Source.java
@@ -11,10 +11,15 @@ public class Source{
 		return a*b;
 	}
 	
+	public static int div(int a, int b){
+		return a/b;
+	}
+	
 	public static void main(String[] Args){
 		int a = 30, b = 10;
 		System.out.println(Source.add(a,b));	
 		System.out.println(Source.sub(a,b));
 		System.out.println(Source.mul(a,b));
+		System.out.println(Source.div(a,b));
 	}
 }
