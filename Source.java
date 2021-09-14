4f3d9dd8423ac389659aabb934da640bad8023be
bpalaniswamy-aie <palaniswamybalaji64@gmail.com>
2021-09-14 10:44:22 +0530

    Source class created

---
+++ Source.java
@@ -0,0 +1,10 @@
+public class Source{
+	public static int add(int a, int b){
+		return a+b;
+	}
+	
+	public static void main(String[] Args){
+		int a = 30, b = 10;
+		System.out.println(Source.add(a,b));	
+	}
+}
